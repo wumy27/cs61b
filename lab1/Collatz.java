214796a7991a6b4d3f13038a8b9c9dd70b95d085
Bevan Wu <46855459+wumy27@users.noreply.github.com>
2023-02-11 19:36:39 +0800

    lab1 first commit

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,14 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+    /** nextNumber of Collatz sequence */
+        if (n == 1) {
             return 1;
-        } else if (n == 5) {
+        } else if (n % 2 == 0) {
+            return n/2;
+        } else {  // (n % 2 == 1)
             return 3 * n + 1;
-        } else {
-            return n * 2;
         }
     }
 
